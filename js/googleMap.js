6dd8a3dad8ff15d5319c5e4d2a32c1890d20fce6
jlopezmazzeira <jlopezmazzeira@gmail.com>
2016-08-27 10:05:29 -0400

    Se coloco el logo mas pequeño para los moviles y se agrego el market al map de google

--- js/googleMap.js
+++ js/googleMap.js
@@ -23,12 +23,22 @@ function initMap() {
     }
   ];
 
-  // Create a map object and specify the DOM element for display.
-  var map = new google.maps.Map(document.getElementById('map'), {
+  var mapOptions = {
       center: myLatLng,
       scrollwheel: false,
       styles: styleArray,
       zoom: 18
+  };
+  // Create a map object and specify the DOM element for display.
+  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+  var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      title:"Hello World!"
   });
+
+  // To add the marker to the map, call setMap();
+  //marker.setMap(map);
 
 }
